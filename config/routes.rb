5fc31f677c181427e75e5041280f974fab3e22d6
Kezz Hall <kerry.hall2009@ntlworld.com>
2015-09-14 12:25:30 +0000

    Added validations and create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
+  #TESTING
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
